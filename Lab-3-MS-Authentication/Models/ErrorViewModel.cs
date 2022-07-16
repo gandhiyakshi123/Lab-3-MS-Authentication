d4f4ea994bb9e963757270b657361c983d5ffad3
Yakshi Gandhi <200482015@student.georgianc.on.ca>
2022-07-16 14:38:15 -0400

    Add project files.

---
+++ Lab-3-MS-Authentication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab_3_MS_Authentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
